73ba9eb72dbc848fb8dc87c0d5eda1ff946a5f7b
damionpaulhart <damionpaulhart@gmail.com>
2015-02-11 15:17:15 -0500

    first commit

---
+++ data - Copy.js
@@ -0,0 +1,40 @@
+[
+  {
+    "id": 1,
+    "content": "item 1<br>start dph",
+    "start": "2014-01-23T00:00:00.000Z"
+  },
+  {
+    "id": 2,
+    "content": "item 2",
+    "start": "2014-01-18T00:00:00.000Z"
+  },
+  {
+    "id": 3,
+    "content": "item 3",
+    "start": "2014-01-21T00:00:00.000Z"
+  },
+  {
+    "id": 4,
+    "content": "item 4",
+    "start": "2014-01-19T11:00:00.000Z",
+    "end": "2014-01-24T11:00:00.000Z"
+  },
+  {
+    "id": 5,
+    "content": "item 5",
+    "start": "2014-01-28T00:00:00.000Z",
+    "type": "point"
+  },
+  {
+    "id": 6,
+    "content": "item 6",
+    "start": "2014-01-26T00:00:00.000Z"
+  },
+  {
+    "start": "2014-01-20T23:00:00.000Z",
+    "content": "Moves Rehearse",
+    "id": "691668b5-98af-9200-e601-42986c97534",
+    "group": null
+  }
+  ]
