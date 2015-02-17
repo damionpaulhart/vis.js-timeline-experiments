1c33b79605d6fbaec6c74b608978a65f27341445
damionpaulhart <damionpaulhart@gmail.com>
2015-02-17 15:52:58 -0500

    updated baranby functionality

---
+++ barbary/data.js
@@ -0,0 +1,64 @@
+[
+  {
+    "id": 1,
+    "content": "item 1<br>start dph",
+    "start": "2014-01-23T00:00:00.000Z"
+  },
+  {
+    "id": 2,
+    "content": "item 2",
+    "start": "2014-01-18T00:00:00.000Z"
+  },
+  {
+    "id": 3,
+    "content": "item 3",
+    "start": "2014-01-21T00:00:00.000Z"
+  },
+  {
+    "id": 4,
+    "content": "item 4",
+    "start": "2014-01-18T11:00:00.000Z",
+    "end": "2014-01-18T23:00:00.000Z"
+  },
+  {
+    "id": 5,
+    "content": "item 5",
+    "start": "2014-01-28T00:00:00.000Z",
+    "type": "point"
+  },
+  {
+    "id": 6,
+    "content": "item 6",
+    "start": "2014-01-26T00:00:00.000Z"
+  },
+  {
+    "start": "2014-01-20T23:00:00.000Z",
+    "content": "Moves Rehearse",
+    "id": "691668b5-98af-9200-e601-42986c97534",
+    "group": null
+  },
+  {
+    "start": "2014-01-23T11:00:00.000Z",
+    "content": "new item",
+    "id": "feb6558d-96a9-a27-3035-379193911d97",
+    "group": null
+  },
+  {
+    "start": "2014-01-24T11:00:00.000Z",
+    "content": "new item",
+    "id": "297b7cfe-348d-44f9-c773-386383693ca1",
+    "group": null
+  },
+  {
+    "start": "2014-01-25T05:00:00.000Z",
+    "content": "new item",
+    "id": "37551534-6391-7423-946e-fb3724777a3f",
+    "group": null
+  },
+  {
+    "start": "2014-01-25T11:00:00.000Z",
+    "content": "new item",
+    "id": "cb1a82cd-79f6-cfb0-7f2d-2c7954fb3216",
+    "group": null
+  }
+]
